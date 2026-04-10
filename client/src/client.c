7ff97414668a4dccc496e0f18394ac77fbc7eb34
Gia <ginetello@frba.utn.ar>
2026-04-10 03:15:18 +0000

    Etapa 2

--- client/src/client.c
+++ client/src/client.c
@@ -1,4 +1,5 @@
 #include "client.h"
+#include <readline/readline.h>
 
 int main(void)
 {
@@ -18,6 +19,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
+	log_info(logger, "Hola! Soy un log");
 
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
@@ -27,8 +29,14 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
-	// Loggeamos el valor de config
+	ip = config_get_string_value(config, "IP"); //te trae el valor de la ip
+	puerto = config_get_string_value(config, "PUERTO"); //el puerto
+	valor = config_get_string_value(config, "CLAVE"); //el valor clave
 
+	// Loggeamos el valor de config
+	log_info(logger, "IP: %s", ip); //IMPORTANTE EL %s para agregar la variable
+	log_info(logger, "Puerto: %s", puerto);
+	log_info(logger, "Clave: %s", valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -54,15 +62,18 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0.log", "CLIENTE", true, LOG_LEVEL_INFO); //true para que lo muestre por pantalla
 
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
-
+	t_config* nuevo_config = config_create("cliente.config"); //creamos el archivo config
+	if(nuevo_config==NULL){
+		//no se puede crear el config, hay que terminar el programa
+		abort();
+	}
 	return nuevo_config;
 }
 
@@ -75,8 +86,13 @@ void leer_consola(t_log* logger)
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
 
-
+	while(leido && strcmp(leido, "") != 0 ){ //si leido no es NULL ni es un caracter vacio
+		log_info(logger, "COnsola: %s", leido);  //lo logea
+		free(leido); //libera la memoria
+		leido = readline("> "); //pide otra linea
+	}
 	// ¡No te olvides de liberar las lineas antes de regresar!
+	free(leido);
 
 }
 
@@ -97,4 +113,7 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+
+	log_destroy(logger); //libera el log para que no haya un memory leak
+	config_destroy(config); //destruye el archivo config
 }
